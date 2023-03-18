9859d6bff5106516b8f67fe4400a0a71cf768cae
JL <2174694433@qq.com>
2023-03-18 10:04:45 +0800

    hot-fox

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("你好2");
         System.out.println("你好3");
         System.out.println("我是新加的hot-fox");
+        System.out.println("hot-fox test");
     }
 }
